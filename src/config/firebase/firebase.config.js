081ed6f8c867f8ddf886f0ff454ed36f9300d985
shohagroy <pkshohag240@gmail.com>
2023-11-05 00:58:58 +0600

    env file config added

--- src/config/firebase/firebase.config.js
+++ src/config/firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCaVnUx1as_7wSIqdTEXKfVLm6IqQeqbIQ",
-  authDomain: "free-chat--app.firebaseapp.com",
-  projectId: "free-chat--app",
-  storageBucket: "free-chat--app.appspot.com",
-  messagingSenderId: "813075595608",
-  appId: "1:813075595608:web:21605a6043e072f75ae52d",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
